ee347cf32ead96fd73a77c735a71a4afbc1c16ca
buleoer <eric.buleon@gmail.com>
2024-07-29 16:57:18 +0000

    3.7: Phonebook backend step 7

--- phonebook/index.js
+++ phonebook/index.js
@@ -1,8 +1,10 @@
 const bodyParser = require('body-parser')
 const express = require('express')
+var morgan = require('morgan')
 
 const app = express()
 app.use(express.json())
+app.use(morgan('tiny'))
 
 let persons = [
     { 
